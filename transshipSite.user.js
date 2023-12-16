678a41832729960c5cecff2428db65ee831bb6a5
Knullack <63751877+Knullack@users.noreply.github.com>
2023-12-16 16:55:25 -0500

    Added check if inventory section is available & count of transhipment rows

--- transshipSite.user.js
+++ transshipSite.user.js
@@ -12,28 +12,50 @@
 (function () {
     'use strict';
     async function extractText() {
-        const tbodyElement = document.querySelector("#table-inventory > tbody");
-        if (!tbodyElement) {
-            console.error('Tbody element not found.');
-            return;
-        }
-        const numberOfRows = tbodyElement.querySelectorAll('tr').length;
-        for (let i = 1; i <= numberOfRows; i++) {
-            const sourceElement = i !== 1
+
+        const inventory_section = document.querySelector("#inventory-status a");
+        if (inventory_section) {
+            const tbodyElement = document.querySelector("#table-inventory > tbody");
+            if (!tbodyElement) {
+                console.error('Inventory table Tbody element not found');
+                return;
+            }
+
+            var r = 1;
+            const rows = document.querySelectorAll("#table-inventory > tbody > tr.odd, #table-inventory > tbody > tr.even");
+            rows.forEach(row => {
+                const transhipElement = row.querySelector('td:nth-child(8)');
+                console.log(`row ${r} : ${transhipElement.textContent.trim()}`);
+                if (transhipElement && transhipElement.textContent.trim() === "UNOWNED") {
+                    console.log(`Stopping tranship search at row ${r-1}`);
+                } else {
+                    r += 1;
+                }
+
+            });
+
+
+
+            const numberOfRows = tbodyElement.querySelectorAll('tr').length;
+            for (let i = 1; i <= r-1; i++) {
+                const sourceElement = i !== 1
                 ? document.querySelector(`#table-inventory > tbody > tr:nth-child(${i}) > td.sorting_1 > a`)
                 : document.querySelector("#table-inventory > tbody > tr > td.sorting_1 > a");
 
-            const transhipElement = i !== 1
+                const transhipElement = i !== 1
                 ? document.querySelector(`#table-inventory > tbody > tr:nth-child(${i}) > td:nth-child(8)`)
                 : document.querySelector("#table-inventory > tbody > tr > td:nth-child(8)");
 
-            if (sourceElement) {
-                const searchText = sourceElement.textContent.trim();
-                const site = await searchOnOtherSite(searchText);
-                transhipElement.innerHTML += ` (${site})`;
-            } else {
-                console.error('container (csX/paX) element not found.');
+                if (sourceElement) {
+                    const searchText = sourceElement.textContent.trim();
+                    const site = await searchOnOtherSite(searchText);
+                    transhipElement.innerHTML += ` (${site})`;
+                } else {
+                    console.error('container (csX/paX) element not found.');
+                }
             }
+        } else {
+            console.log('No inventory Section Found');
         }
     }
     function searchOnOtherSite(searchText) {
